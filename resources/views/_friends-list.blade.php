88f92c109d86fa264be539a6e18ac2cdfcf9faa1
mattmannec <mattmannec@gmail.com>
2020-08-26 15:35:09 +0200

    finished add-friends

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,13 +1,13 @@
 <div class="bg-gray-200 rounded-lg py-4 px-6">
-    <h3 class="font-bold text-xl mb-4">Friends</h3>
+    <h3 class="font-bold text-xl mb-4">Following</h3>
 
     <ul>
-        @foreach (range(1, 8) as $index)
+        @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
-                <img src="http://lorempixel.com/40/40/people/" alt="" class="rounded-full mr-2">
+                <img src="{{ $user->avatar }}" alt="" class="rounded-full mr-2">
 
-                John Doe
+                {{ $user->name }}
             </div>
         </li>
         @endforeach
