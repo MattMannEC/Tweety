64deaf38adafad029300e58405fef7e6143d002d
mattmannec <mattmannec@gmail.com>
2020-08-26 10:15:45 +0200

    cloned from github

---
+++ resources/views/_sidebar-links.blade.php
@@ -0,0 +1,26 @@
+<ul>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">Home</a>
+    </il>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">Explore</a>
+    </il>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">Notifications</a>
+    </il>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">Messages</a>
+    </il>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">Bookmarks</a>
+    </il>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">Lists</a>
+    </il>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">Profile</a>
+    </il>
+    <il>
+        <a href="" class="font-bold text-lg mb-4 block">More</a>
+    </il>
+</ul>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="lg:flex">
+    <div class="lg:w-1/6">
+        @include('_sidebar-links')
+    </div>
+    <div class="lg:flex-1 lg:mx-10">
+        <div class="border border-blue-400 rounded-lg p-8">
+            <form action="">
+                <textarea name="body" id="" class="w-full" placeholder="Say Something">
+                </textarea>
+                <hr>
+                <button type="submit">Tweet</button>
+            </form>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
         </div>
+        <div>
+
+        </div>
+    </div>
+    <div class="lg:w-1/6">
+        @include('_friends-links')
     </div>
 </div>
 @endsection
